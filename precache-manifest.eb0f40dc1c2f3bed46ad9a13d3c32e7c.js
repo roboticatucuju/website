8fbd867e413c82a09c671f5e95260aa25bbe7567
Mateus Bezerra <mateusbzerra@gmail.com>
2019-08-13 10:10:42 -0300

    Componente Sobre finalizado, protótipo em funcionamento

---
+++ precache-manifest.eb0f40dc1c2f3bed46ad9a13d3c32e7c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e1e7266415a445002d1008fa96fbdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a58318e81d4e21be70cd",
+    "url": "/static/css/main.c8890ae0.chunk.css"
+  },
+  {
+    "revision": "043edab3d0268807a9c5",
+    "url": "/static/js/2.1bbde210.chunk.js"
+  },
+  {
+    "revision": "a58318e81d4e21be70cd",
+    "url": "/static/js/main.40fb5cc4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "818bb68aa625bd8bb72f35043b28960d",
+    "url": "/static/media/about.818bb68a.svg"
+  },
+  {
+    "revision": "fa496593cc46664658c252cbfca09942",
+    "url": "/static/media/cover.fa496593.jpg"
+  },
+  {
+    "revision": "b6086b7307eb3566e63798a4e14986d0",
+    "url": "/static/media/header.b6086b73.svg"
+  },
+  {
+    "revision": "005f13d067d422de0bfd845b622d9806",
+    "url": "/static/media/service.005f13d0.svg"
+  }
+]);
